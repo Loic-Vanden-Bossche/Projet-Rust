60eebcafae724234b7a5307deb32508506d5e699
Denis Turbiez <denisft77@gmail.com>
2023-01-03 12:23:16 +0100

    connect to server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#![allow(non_snake_case)]
-
+use std::arch::x86_64::_mm_rcp_ps;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
@@ -12,24 +11,42 @@ struct Version {
 	version: i32
 }
 
-impl Version {
-	/*fn to_string(&self) ->String {
-		format!("{{\"Version\":{}}}", self.Version)
-	}*/
-}
-
 #[derive(Serialize, Deserialize, Debug)]
 struct Welcome {
 	Welcome: Version
 }
 
-impl Welcome {
-	/*fn to_string(&self) -> String {
-		format!("{{\"welcome\":{}}}", self.welcome.to_string())
-	}*/
+#[derive(Serialize, Deserialize, Debug)]
+struct Name{
+	name: String
 }
 
-fn read_from_stream(mut stream: TcpStream){
+#[derive(Serialize, Deserialize, Debug)]
+struct Subscribe {
+	Subscribe: Name
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum SubscribeError{
+	AlreadyRegistered, InvalidName
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum SubscribeResultEnum{
+	Ok,
+	Err(SubscribeError)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SubscribeResult{
+	SubscribeResult: SubscribeResultEnum
+}
+
+struct Error{
+	coucou: i32
+}
+
+fn read_from_stream<T:for<'a> Deserialize<'a>>(mut stream: &TcpStream) -> Result<T, Error>{
 	match stream.read_u32::<BigEndian>() {
 		Ok(size_r) => {
 			println!("Size: {}", &size_r);
@@ -37,32 +54,70 @@ fn read_from_stream(mut stream: TcpStream){
 			match stream.read_exact(& mut buf) {
 				Ok(_) => {
 					let text = from_utf8(&buf).unwrap();
-					println!("{}", text);
-					let test2: Welcome = serde_json::from_str(text).unwrap();
-					println!("{}", serde_json::to_string(&test2).unwrap());
-
+					Ok(serde_json::from_str(text).unwrap())
 				}
 				Err(e) => {
 					println!("Erreur à la lecture: {}", e);
+					Err(Error{ coucou: 1})
 				}
 			}
 		}
 		Err(e) => {
 			println!("Failed to read data: {}", e);
+			Err(Error{ coucou: 2})
 		}
 	}
+}
+
+fn write_to_stream(mut stream: &TcpStream, message: String){
+	let mut size: u32;
+	match u32::try_from(message.len()) {
+		Ok(val) => {
+			size = val;
+		}
+		Err(_) => {
+			println!("Erreur message trop long");
+			return;
+		}
+	}
+	let size_b = size.to_be_bytes();
+	stream.write(&size_b).expect("AHHHH");
+	let mess = message.into_bytes();
+	stream.write(&mess).expect("OULA");
 }
 
 fn main() {
 	let stream = TcpStream::connect("127.0.0.1:7878");
 	let hello = b"\"Hello\"";
-	let size_w = 7_u32.to_be_bytes();
+	let mut size_w = 7_u32.to_be_bytes();
 	match stream {
 		Ok(mut stream) => {
 			println!("Connected");
 			stream.write(&(size_w)).expect("PANIKKK");
 			stream.write(hello).expect("PANIKKK");
-			read_from_stream(stream)
+			let welcome: Result<Welcome, Error> = read_from_stream(&stream);
+			match welcome {
+				Ok(val) => {
+					if val.Welcome.version == 1 {
+						let sub = Subscribe{Subscribe: Name{ name: "macaron".to_string() }};
+						write_to_stream(&stream, serde_json::to_string(&sub).unwrap());
+						let res: Result<SubscribeResult, Error> = read_from_stream(&stream);
+						match res {
+							Ok(val) => {
+								println!("{}", serde_json::to_string(&val).unwrap())
+							}
+							Err(_) => {
+								println!("D'où")
+							}
+						}
+					}else{
+						println!("Unsupported version");
+					}
+				}
+				Err(_) => {
+					println!("MERDE")
+				}
+			}
 		}
 		Err(_) => {
 			println!("Failed to connect");
