3e0a934fc23649234e57be48bd1586dc48be8c6b
Denis Turbiez <denisft77@gmail.com>
2023-01-03 16:15:16 +0100

    refactor connect

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![allow(non_snake_case)]
 
+use std::env;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 use byteorder::{BigEndian, ReadBytesExt};
 
 use serde::{Serialize, Deserialize};
+use crate::SubscribeError::InvalidName;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Version {
@@ -27,18 +29,18 @@ struct Subscribe {
 	Subscribe: Name
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 enum SubscribeError{
 	AlreadyRegistered, InvalidName
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 enum SubscribeResultEnum{
 	Ok,
 	Err(SubscribeError)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 struct SubscribeResult{
 	SubscribeResult: SubscribeResultEnum
 }
@@ -50,7 +52,6 @@ struct Error{
 fn read_from_stream<T:for<'a> Deserialize<'a>>(mut stream: &TcpStream) -> Result<T, Error>{
 	match stream.read_u32::<BigEndian>() {
 		Ok(size_r) => {
-			println!("Size: {}", &size_r);
 			let mut buf = vec![0u8; size_r as usize];
 			match stream.read_exact(& mut buf) {
 				Ok(_) => {
@@ -87,52 +88,80 @@ fn write_to_stream(mut stream: &TcpStream, message: String){
 	stream.write(&mess).expect("OULA");
 }
 
-fn connect() -> Result<TcpStream, Error>{
-	let stream = TcpStream::connect("127.0.0.1:7878");
-	let hello = b"\"Hello\"";
-	let size_w = 7_u32.to_be_bytes();
-	match stream {
-		Ok(mut stream) => {
-			println!("Connected");
-			stream.write(&(size_w)).expect("PANIKKK");
-			stream.write(hello).expect("PANIKKK");
-			let welcome: Result<Welcome, Error> = read_from_stream(&stream);
-			match welcome {
+fn connect(ip: String, name: String) -> Result<TcpStream, Error>{
+	let res_stream = TcpStream::connect(ip);
+	let stream = match res_stream {
+		Ok(res) => {
+			res
+		}
+		Err(_) => {
+			println!("Failed to connect");
+			return Err(Error{coucou: 4});
+		}
+	};
+	write_to_stream(&stream, "\"Hello\"".to_string());
+	let welcome: Result<Welcome, Error> = read_from_stream(&stream);
+	let sub_res = match welcome {
+		Ok(val) => {
+			if val.Welcome.version != 1 {
+				println!("Unsupported version");
+				return Err(Error{coucou: 4});
+			}
+			let sub = Subscribe{Subscribe: Name{ name }};
+			write_to_stream(&stream, serde_json::to_string(&sub).unwrap());
+			let res: Result<SubscribeResult, Error> = read_from_stream(&stream);
+			match res {
 				Ok(val) => {
-					if val.Welcome.version == 1 {
-						let sub = Subscribe{Subscribe: Name{ name: "macaron".to_string() }};
-						write_to_stream(&stream, serde_json::to_string(&sub).unwrap());
-						let res: Result<SubscribeResult, Error> = read_from_stream(&stream);
-						match res {
-							Ok(val) => {
-								println!("{}", serde_json::to_string(&val).unwrap());
-								Ok(stream)
-							}
-							Err(_) => {
-								println!("D'où");
-								Err(Error{coucou: 4})
-							}
-						}
-					}else{
-						println!("Unsupported version");
-						Err(Error{coucou: 4})
-					}
+					val
 				}
 				Err(_) => {
-					println!("MERDE");
-					Err(Error{coucou: 4})
+					return Err(Error{coucou: 5})
 				}
 			}
 		}
 		Err(_) => {
-			println!("Failed to connect");
-			Err(Error{coucou: 4})
+			println!("MERDE");
+			return Err(Error{coucou: 4});
+		}
+	};
+	match sub_res.SubscribeResult {
+		SubscribeResultEnum::Ok => {
+			Ok(stream)
+		}
+		SubscribeResultEnum::Err(error) => {
+			return match error {
+				InvalidName => {
+					println!("Invalid Name");
+					Err(Error { coucou: 5 })
+				}
+				_AlreadyRegistered => {
+					Err(Error { coucou: 6 })
+				}
+			}
 		}
 	}
+
 }
 
 fn main() {
-	match connect() {
+	let args: Vec<String> = env::args().collect();
+	let mut name = "macaron".to_string();
+	let mut ip = "127.0.0.1:7878".to_string();
+	let mut next = 0;
+	for arg in args {
+		if arg == "--name" {
+			next = 1;
+		}
+		if arg == "--ip" {
+			next = 2;
+		}
+		if next == 1 {
+			name = arg;
+		}else if next == 2 {
+			ip = arg
+		}
+	}
+	match connect(ip, name) {
 		Ok(_) => {
 			println!("Connecté");
 		}
