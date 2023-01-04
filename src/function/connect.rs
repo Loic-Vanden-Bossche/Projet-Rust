bc05d6adc29e4fb02eed8f3a8900525a9ae54d64
Denis Turbiez <denisft77@gmail.com>
2023-01-04 12:22:09 +0100

    refacto connect to server

---
+++ src/function/connect.rs
@@ -0,0 +1,93 @@
+use std::net::TcpStream;
+use serde::de::value::BoolDeserializer;
+use serde_json::to_string;
+use crate::function::stream::{read_from_stream, write_to_stream};
+use crate::types::error::{Error, ReadError};
+use crate::types::subscribe::{Name, Subscribe, SubscribeError, SubscribeResult, SubscribeResultEnum};
+use crate::types::welcome::Welcome;
+
+fn get_stream(ip: String) -> Result<TcpStream, Error>{
+    return match TcpStream::connect(ip) {
+        Ok(s) => {
+            Ok(s)
+        }
+        Err(_) => {
+            println!("Failed to connect");
+            Err(Error{coucou: 4})
+        }
+    };
+}
+
+fn hello(stream: &TcpStream) -> bool {
+    write_to_stream(&stream, "\"Hello\"".to_string());
+    return match read_from_stream(&stream){
+        Ok(val) => {
+            let val: Welcome = val;
+            if val.version == 1 {
+                true
+            }else{
+                println!("Unsupported version");
+                false
+            }
+        }
+        Err(_) => {
+            println!("Error while hello");
+            false
+        }
+    }
+}
+
+fn subscribe(stream: &TcpStream, name: String) -> Result<i32, Error> {
+    let sub = Subscribe{Subscribe: Name{ name }};
+    write_to_stream(&stream, to_string(&sub).unwrap());
+    let sub_res: SubscribeResult = match read_from_stream(&stream) {
+        Ok(val) => {
+            val
+        }
+        Err(_) => {
+            return Err(Error{coucou: 5})
+        }
+    };
+    match sub_res.SubscribeResult {
+        SubscribeResultEnum::Ok => {
+            Ok(1)
+        }
+        SubscribeResultEnum::Err(err) => {
+            match err {
+                SubscribeError::InvalidName => {
+                    println!("Invalid Name");
+                    Err(Error { coucou: 5 })
+                }
+                SubscribeError::AlreadyRegistered => {
+                    println!("Already registered");
+                    Err(Error{coucou: 6})
+                }
+            }
+        }
+    }
+}
+
+pub fn connect(ip: String, name: String) -> Result<TcpStream, Error>{
+    let stream = match get_stream(ip) {
+        Ok(s) => {
+            s
+        }
+        Err(err) => {
+            println!("Failed to connect");
+            return Err(err);
+        }
+    };
+    if hello(&stream){
+        match subscribe(&stream, name) {
+            Ok(_) => {
+                Ok(stream)
+            }
+            Err(err) => {
+                println!("Error while subscribing");
+                Err(err)
+            }
+        }
+    }else{
+        Err(Error{coucou: 5})
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,71 +4,13 @@ mod types;
 mod function;
 
 use std::env;
-use std::net::TcpStream;
+use crate::function::connect::connect;
 
 use crate::function::stream::{read_from_stream, write_to_stream};
 use crate::types::challenge::{Challenge, ChallengeAnswer, ChallengeResult, ChallengeResultData, MD5HashCashOutput};
 use crate::types::end::EndOfGame;
-use crate::types::error::{Error, ReadError};
 use crate::types::player::{PublicLeaderBoard};
 use crate::types::round::RoundSummary;
-use crate::types::subscribe::{Name, Subscribe, SubscribeError, SubscribeResult, SubscribeResultEnum};
-use crate::types::welcome::Welcome;
-
-fn connect(ip: String, name: String) -> Result<TcpStream, Error>{
-	let res_stream = TcpStream::connect(ip);
-	let stream = match res_stream {
-		Ok(res) => {
-			res
-		}
-		Err(_) => {
-			println!("Failed to connect");
-			return Err(Error{coucou: 4});
-		}
-	};
-	write_to_stream(&stream, "\"Hello\"".to_string());
-	let welcome: Result<Welcome, ReadError> = read_from_stream(&stream);
-	let sub_res = match welcome {
-		Ok(val) => {
-			if val.Welcome.version != 1 {
-				println!("Unsupported version");
-				return Err(Error{coucou: 4});
-			}
-			let sub = Subscribe{Subscribe: Name{ name }};
-			write_to_stream(&stream, serde_json::to_string(&sub).unwrap());
-			let res: Result<SubscribeResult, ReadError> = read_from_stream(&stream);
-			match res {
-				Ok(val) => {
-					val
-				}
-				Err(_) => {
-					return Err(Error{coucou: 5})
-				}
-			}
-		}
-		Err(_) => {
-			println!("MERDE");
-			return Err(Error{coucou: 4});
-		}
-	};
-	match sub_res.SubscribeResult {
-		SubscribeResultEnum::Ok => {
-			Ok(stream)
-		}
-		SubscribeResultEnum::Err(error) => {
-			return match error {
-				SubscribeError::InvalidName => {
-					println!("Invalid Name");
-					Err(Error { coucou: 5 })
-				}
-				SubscribeError::AlreadyRegistered => {
-					println!("Already registered");
-					Err(Error{coucou: 6})
-				}
-			}
-		}
-	}
-}
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
