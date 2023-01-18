83489ef6754f1ddb618b1d58b54282edb2380254
Loïc Vanden Bossche <loloic066@gmail.com>
2023-01-19 00:20:54 +0100

    Monstrous maze now uses dijkstra

--- src/challenges/monstrous_maze/challenge.rs
+++ src/challenges/monstrous_maze/challenge.rs
@@ -1,8 +1,9 @@
-use std::collections::VecDeque;
 use simplelog::debug;
 use crate::challenges::monstrous_maze::types::input::MonstrousMazeInput;
 use crate::challenges::monstrous_maze::types::map::Map;
 use crate::challenges::monstrous_maze::types::output::MonstrousMazeOutput;
+use std::collections::{BinaryHeap, HashMap};
+
 
 fn parse_input(input: MonstrousMazeInput) -> Map {
     let mut maze = Vec::new();
@@ -30,38 +31,18 @@ fn parse_input(input: MonstrousMazeInput) -> Map {
 }
 
 fn find_path(map: &Map) -> Option<String> {
-    let mut queue = VecDeque::new();
-    queue.push_back((map.player.0, map.player.1));
-
-    let mut visited = vec![vec![false; map.maze[0].len()]; map.maze.len()];
-    visited[map.player.0][map.player.1] = true;
+    let mut distances = HashMap::new();
+    let mut heap = BinaryHeap::new();
+    let mut prev = HashMap::new();
 
     let dx = [1, 0, -1, 0];
     let dy = [0, 1, 0, -1];
 
-    let mut prev = vec![vec![(0, 0); map.maze[0].len()]; map.maze.len()];
+    distances.insert((map.player.0, map.player.1), 0);
+    heap.push((map.player.0, map.player.1));
 
-    while let Some((x, y)) = queue.pop_front() {
-        if (x, y) == map.exit {
-            let mut path = String::new();
-            let mut curr = (x, y);
-            while curr != map.player {
-                let (dx, dy) = (curr.0 as isize - prev[curr.0][curr.1].0 as isize,
-                                curr.1 as isize - prev[curr.0][curr.1].1 as isize);
-                if dx == 1 {
-                    path.push('v');
-                } else if dx == -1 {
-                    path.push('^');
-                } else if dy == 1 {
-                    path.push('>');
-                } else if dy == -1 {
-                    path.push('<');
-                }
-                curr = prev[curr.0][curr.1];
-            }
-            path = path.chars().rev().collect::<String>();
-            return Some(path);
-        }
+    while let Some((x, y)) = heap.pop() {
+        let distance = *distances.get(&(x, y)).unwrap();
 
         for i in 0..4 {
             let nx = x as isize + dx[i];
@@ -71,21 +52,47 @@ fn find_path(map: &Map) -> Option<String> {
                 let nx = nx as usize;
                 let ny = ny as usize;
 
-                if map.maze[nx][ny] != '#' && !visited[nx][ny] {
-                    visited[nx][ny] = true;
-                    prev[nx][ny] = (x, y);
-                    queue.push_back((nx, ny));
+                let new_distance = distance + 1;
+
+                if map.maze[nx][ny] != '#' && !distances.contains_key(&(nx, ny)) {
+                    distances.insert((nx, ny), new_distance);
+                    prev.insert((nx, ny), (x, y));
+                    heap.push((nx, ny));
+                } else if map.maze[nx][ny] != '#' && new_distance < *distances.get(&(nx, ny)).unwrap() {
+                    distances.insert((nx, ny), new_distance);
+                    prev.insert((nx, ny), (x, y));
+                    heap.push((nx, ny));
                 }
             }
         }
     }
 
-    None
+    let mut path = String::new();
+    let mut current = (map.exit.0, map.exit.1);
+
+    while current != (map.player.0, map.player.1) {
+        let prev = prev.get(&current).unwrap();
+        let dx = current.0 as isize - prev.0 as isize;
+        let dy = current.1 as isize - prev.1 as isize;
+
+        if dx == 1 {
+            path.push('v');
+        } else if dx == -1 {
+            path.push('^');
+        } else if dy == 1 {
+            path.push('>');
+        } else if dy == -1 {
+            path.push('<');
+        }
+
+        current = *prev;
+    }
+
+    Some(path.chars().rev().collect::<String>())
 }
 
 pub fn monstrous_maze(input: MonstrousMazeInput) -> MonstrousMazeOutput {
     let map = parse_input(input);
-
     if let Some(path) = find_path(&map) {
         debug!("Path found: {}", path);
         MonstrousMazeOutput {
@@ -97,3 +104,4 @@ pub fn monstrous_maze(input: MonstrousMazeInput) -> MonstrousMazeOutput {
         }
     }
 }
+
