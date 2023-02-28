10b2585a26864be4c3eeb5535b4a9e3469687926
Denis Turbiez <denisft77@gmail.com>
2023-02-28 13:39:35 +0100

    last fix

--- src/challenges/hash_cash/challenge.rs
+++ src/challenges/hash_cash/challenge.rs
@@ -11,8 +11,8 @@ pub fn hash_cash(input: MD5HashCashInput) -> MD5HashCashOutput {
     loop {
         seed = rng.gen::<u64>();
         let digest = md5::compute(format!("{seed:016x}{}", input.message.clone()));
-        println!("{}, {}", count0(digest), input.complexity);
-        if count0(digest) >= input.complexity as u8 {
+        if count0(digest) >= input.complexity {
+            debug!("{:x}", digest);
             answer = format!("{:x}", digest);
             break;
         }
@@ -20,17 +20,11 @@ pub fn hash_cash(input: MD5HashCashInput) -> MD5HashCashOutput {
     MD5HashCashOutput { seed, hashcode: answer }
 }
 
-fn count0(digest: Digest) -> u8 {
-    let mut tot: u8 = 0;
-    for i in 0..digest.0.len() {
-        let mut tmp = 128;
-        while digest[i] < tmp {
-            tot += 1;
-            tmp /= 2;
-        }
-        if tmp != 1 {
-            break;
-        }
-    }
-    return tot;
+fn count0(digest: Digest) -> u32 {
+    let str = format!("{:x}", digest);
+    let value = match u128::from_str_radix(str.as_str(), 16) {
+        Ok(val) => { val }
+        Err(_) => { 0 }
+    };
+    return value.leading_zeros()
 }
